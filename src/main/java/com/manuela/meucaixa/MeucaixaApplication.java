b1f2258d6bad4df28c6c67bbeb2220f3bfb6bccb
manupicoli <manuelaantunes004@gmail.com>
2025-09-23 19:01:17 -0300

    first commit

---
+++ src/main/java/com/manuela/meucaixa/MeucaixaApplication.java
@@ -0,0 +1,13 @@
+package com.manuela.meucaixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeucaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeucaixaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuela/meucaixa/MeucaixaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuela.meucaixa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeucaixaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
