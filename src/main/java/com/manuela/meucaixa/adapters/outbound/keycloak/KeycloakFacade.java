ba6ce0205f874ebe0adf408dcc184a9a7ae37616
manupicoli <manuelaantunes004@gmail.com>
2025-10-19 13:21:33 -0300

    fix create user

--- src/main/java/com/manuela/meucaixa/adapters/outbound/keycloak/KeycloakFacade.java
+++ src/main/java/com/manuela/meucaixa/adapters/outbound/keycloak/KeycloakFacade.java
@@ -43,6 +43,7 @@ public class KeycloakFacade {
                               final String password) {
 
         final var req = CreateKeycloakUserRequest.builder()
+            .email(username)
             .username(username)
             .firstName(firstName)
             .lastName(lastName)
