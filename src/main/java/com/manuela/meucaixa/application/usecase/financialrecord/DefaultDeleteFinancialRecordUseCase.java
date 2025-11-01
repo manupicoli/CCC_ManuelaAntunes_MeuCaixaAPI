a6038d13d31915a4c1d9f3923c1342505491a20f
manupicoli <manuelaantunes004@gmail.com>
2025-11-01 18:56:24 -0300

    implementing delete use case

--- src/main/java/com/manuela/meucaixa/application/usecase/financialrecord/DefaultDeleteFinancialRecordUseCase.java
+++ src/main/java/com/manuela/meucaixa/application/usecase/financialrecord/DefaultDeleteFinancialRecordUseCase.java
@@ -1,15 +1,25 @@
 package com.manuela.meucaixa.application.usecase.financialrecord;
 
+import com.manuela.meucaixa.application.usecase.DomainException;
+import com.manuela.meucaixa.domain.financialrecord.FinancialRecordRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class DefaultDeleteFinancialRecordUseCase implements DeleteFinancialRecordUseCase {
 
+    private final FinancialRecordRepository financialRecordRepository;
+
     @Override
     public void execute(final Long id) {
+        final var financialRecord = financialRecordRepository.findById(id)
+            .orElseThrow(() -> new DomainException("Financial record not found"));
 
+        financialRecordRepository.deleteById(financialRecord.getId());
+        log.info("Financial record successfully deleted");
     }
 }
