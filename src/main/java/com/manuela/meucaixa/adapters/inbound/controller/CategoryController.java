0c8e5fdd4540515b731886e7a506bcd3afe984f6
manupicoli <manuelaantunes004@gmail.com>
2025-11-04 20:35:49 -0300

    adjusting controller

--- src/main/java/com/manuela/meucaixa/adapters/inbound/controller/CategoryController.java
+++ src/main/java/com/manuela/meucaixa/adapters/inbound/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.manuela.meucaixa.adapters.inbound.controller;
 
+import com.manuela.meucaixa.application.usecase.category.AddEditCategoryRequest;
+import com.manuela.meucaixa.application.usecase.category.AddEditCategoryUseCase;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,4 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class CategoryController implements CategoryControllerApi {
+
+    private final AddEditCategoryUseCase addEditCategoryUseCase;
+
+    @Override
+    public void create(AddEditCategoryRequest request) {
+        addEditCategoryUseCase.execute(null, request);
+    }
+
+    @Override
+    public void update(Long id, AddEditCategoryRequest request) {
+        addEditCategoryUseCase.execute(id, request);
+    }
 }
--- src/main/java/com/manuela/meucaixa/adapters/inbound/controller/CategoryControllerApi.java
+++ src/main/java/com/manuela/meucaixa/adapters/inbound/controller/CategoryControllerApi.java
@@ -1,10 +1,21 @@
 package com.manuela.meucaixa.adapters.inbound.controller;
 
+import com.manuela.meucaixa.application.usecase.category.AddEditCategoryRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RequestMapping("/v1/category")
 public interface CategoryControllerApi {
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    void create(@Valid @RequestBody AddEditCategoryRequest request);
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void update(@PathVariable Long id,
+                @Valid @RequestBody AddEditCategoryRequest request);
 }
